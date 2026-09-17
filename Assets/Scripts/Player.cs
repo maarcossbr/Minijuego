7aacb60bc0390237818da15783baf29a8ce4f087
maarcossbr <marcosbarraldelgado@gmail.com>
2026-09-17 12:55:42 +0200

    Proyecto inicial Unity

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float thrustForce = 5f;
+    public float rotationSpeed = 10f;
+
+    private Rigidbody _rigid;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float rotation = Input.GetAxis("Horizontal") * Time.deltaTime;
+        float thrust = Input.GetAxis("Vertical") * Time.deltaTime;
+
+        Vector3 thrustDirection = transform.right;
+
+        _rigid.AddForce(thrustDirection * thrust * thrustForce);
+
+        transform.Rotate(Vector3.forward, -rotation * rotationSpeed);
+    }
+}
