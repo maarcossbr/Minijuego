f60fca2b081da1b543235bdbf25e97654c235ca1
maarcossbr <marcosbarraldelgado@gmail.com>
2026-09-17 15:21:31 +0200

    Añadido movimiento y disparos

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 10f;
+    public float maxLifeTime = 3f;
+    public Vector3 targetVector;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(gameObject, maxLifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speed * targetVector * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,11 @@ public class Player : MonoBehaviour
     public float thrustForce = 5f;
     public float rotationSpeed = 10f;
 
+    public GameObject gun, bulletPrefab; 
+
     private Rigidbody _rigid;
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,5 +30,13 @@ public class Player : MonoBehaviour
         _rigid.AddForce(thrustDirection * thrust * thrustForce);
 
         transform.Rotate(Vector3.forward, -rotation * rotationSpeed);
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            GameObject bullet = Instantiate(bulletPrefab, gun.transform.position, Quaternion.identity);
+
+            Bullet balaScript = bullet.GetComponent<Bullet>();
+
+            balaScript.targetVector = transform.right;
+        }
     }
 }
