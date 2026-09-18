ec1f2fef7e7d667311a8cd23e61ef574f9d0db43
maarcossbr <marcosbarraldelgado@gmail.com>
2026-09-18 11:50:27 +0200

    Menu de pausa

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    private bool pausado = false;
+
+    void Start()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pausado)
+                Reanudar();
+            else
+                Pausar();
+        }
+    }
+
+    public void Pausar()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        pausado = true;
+    }
+
+    public void Reanudar()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        pausado = false;
+    }
+
+    public void Reiniciar()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+        pausado = false;
+        Player.SCORE = 0;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
